958e83d9e6f1d4a80c142ffa1d1fff0a65b4fcca
Fabricio Sousa <fabricio_370@hotmail.com>
2023-04-03 23:26:18 +0100

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 //Method 1 : Using Async Await
 
@@ -19,43 +19,38 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     const myRecipe = {
-      title: 'Bolonhesa',
-      level: 'Easy Peasy',
-      ingredients: ['Esparguete', 'Carne'],
-      cuisine: 'italian',
-      dishType: 'main_course',
+      title: "Bolonhesa",
+      level: "Easy Peasy",
+      ingredients: ["Esparguete", "Carne"],
+      cuisine: "italian",
+      dishType: "main_course",
       image: "https://images.media-allrecipes.com/images/75131.jpg",
       duration: 15,
-      creator: 'Fabrício',
+      creator: "Fabrício",
     };
     const createdRecipe = await Recipe.create(myRecipe);
-    console.log(createdRecipe.title)
+    console.log(createdRecipe.title);
 
     const multipleRecipes = await Recipe.insertMany(data);
-    multipleRecipes.forEach(recipe => {
-      console.log('Title --->', recipe.title)
-    })
-  
+    multipleRecipes.forEach((recipe) => {
+      console.log("Title --->", recipe.title);
+    });
+
     Recipe.findOneAndUpdate(
-      {title: 'Rigstoni alla Genovese'},
-      {duration: 100},
-      {new: true}
+      { title: "Rigstoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
     );
 
-    await Recipe.deleteOne({title: 'Carrot Cake'});  
-
+    await Recipe.deleteOne({ title: "Carrot Cake" });
   } catch (error) {
     console.log(error);
   } finally {
-    await mongoose.connection.close()
+    await mongoose.connection.close();
   }
 };
 
 manageRecipes();
-
-
-
-
 
 //Method 2: Using .then() method
 //If you want to use this method uncomment the code below:
@@ -73,4 +68,3 @@ manageRecipes();
   .catch((error) => {
     console.error('Error connecting to the database', error);
   }); */
-
